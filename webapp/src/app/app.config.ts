7271289a68382e1019dcaca0551f73956a7b8a93
stba <stefan.baust@stefanbaust.de>
2026-02-27 18:12:52 +0100

    first version angular

--- webapp/src/app/app.config.ts
+++ webapp/src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+
+registerLocaleData(localeDe);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    
+    { provide: LOCALE_ID, useValue: 'de-DE' },
   ]
 };
